0e7e49995944284cf49c47f8624232958d35d3fc
TrueGamerTalha <69008390+TrueGamerTalha@users.noreply.github.com>
2020-12-07 19:34:16 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+
 var dbd = require("dbd.js");
 var fs = require("fs");
 var bot = new dbd.Bot({
-  token: "SPOIDY_TOKEN",
+  token: "NzQ5MTMzNDM1Nzc5NTQ3MTg4.X0ni4Q.QL37K5U0v0D6JZJSB6Xpv_4R0uo",
   prefix: "S!"
 });
 
@@ -22,65 +23,93 @@ for (const file of reader) {
 }
 
 bot.command({
+  name: "hello",
+  code: `Hi! I am **Spoidy**!`
+})
+
+bot.command({
+  name: "afk",
+  code: `$setUserVar[afk;true] $setUserVar[afkwhy;$noMentionMessage]
+**$username** You Are Now AFK For $noMentionMessage`
+});
+
+bot.command({
+  name: "$alwaysExecute",
+  code: `**$username** You are now no longer AFK! 
+$setUserVar[afk;false]
+$setUserVar[afkwhy;No reason given.]
+$onlyIf[$getUserVar[afk]==true;]`
+});
+
+bot.command({
+  name: "$alwaysExecute",
+  code: `
+**$username[$mentioned[1]]** is Currently AFK For $getUserVar[afkwhy;$mentioned[1]]
+$onlyIf[$getUserVar[afk;$mentioned[1]]==true;]`,
+  nonPrefixed: true
+});
+
+bot.command({
   name: "<@749133435779547188>",
   code: `$color[b200ff]
 $title[My Prefix In This Server Is  **S!**]`,
   nonPrefixed: true
 });
 
+bot.variables({
+  warn: "0",
+  afk: "false",
+  afkwhy: "No reason given."
+});
 bot.command({
   name: "help",
   code: `$reactionCollector[$splitText[1];everyone;1m;1️⃣,2️⃣,3️⃣;awaitReaction1,awaitReaction2,awaitReaction3,awaitReaction4;yes]
-$textSplit[$sendMessage[{title: Spoidy Help Commands} {description:
+$textSplit[$sendMessage[{title: Spoidy Misc. Commands} {description:
+**S!help** 
+Shows The Help Commands
 
-**S!warn <user>**
-Warns A Member
- 
-**S!mute <user>**
-Mutes A user
+**S!info**
+Shows Spoidy's Stats
 
-**S!unmute <user>**
-Unmutes a user
+**S!support**
+Gives Link To Support Server
 
-**S!tempmute <user> <time>**
-Temporarily Mutes a User
+**S!invite**
+Gives Invite Link For Spoidy
 
-**S!ban <user>**
-Bans A User From The Server
+**S!github**
+Gives The GitHub Link To Spoidy
 
-**S!unban <userID>**
-Unbans A User From The Server
+**S!reminder <time> <message>**
+Reminds You In The Given Time
 
-**S!kick <user>**
-Kicks A User From The Server
-
-**S!clear <amount>**
-Clears an Amount Of Messages} {color:b200ff};yes]; ]`
+**S!afk <reason>**
+Sets AFK For User} {color:b200ff};yes]; ]`
 });
 
 bot.awaitedCommand({
   name: "awaitReaction1",
-  code: `$editMessage[$message[1];{title:Spoidy Moderation Commands} {description:
-**S!warn <user>**
-Warns A Member
- 
-**S!mute <user>**
-Mutes A user
+  code: `$editMessage[$message[1];{title:Spoidy Misc. Commands} {description:
+**S!help** 
+Shows The Help Commands
 
-**S!unmute <user>**
-Unmutes a user
+**S!info**
+Shows Spoidy's Stats
 
-**S!ban <user>**
-Bans A User From The Server
+**S!support**
+Gives Link To Support Server
 
-**S!unban <userID>**
-Unbans A User From The Server
+**S!invite**
+Gives Invite Link For Spoidy
 
-**S!kick <user>**
-Kicks A User From The Server
+**S!github**
+Gives The GitHub Link To Spoidy
 
-**S!clear <amount>**
-Clears an Amount Of Messages} {color:b200ff}
+**S!reminder <time> <message>**
+Reminds You In The Given Time
+
+**S!afk <reason>**
+Sets AFK For User} {color:b200ff}
 ]
 `
 });
@@ -88,11 +117,41 @@ Clears an Amount Of Messages} {color:b200ff}
 bot.awaitedCommand({
   name: "awaitReaction2",
   code: `
- $editMessage[$message[1];{title:Spoidy Fun Commands} {description:
- **S!lovecheck <@user>**
-Check Love Between You And The User
+ $editMessage[$message[1];{title:Spoidy Moderation Commands} {description:
+**S!warn <user>**
+Warns A Member
+ 
+**S!mute <user>**
+Mutes A user
 
-**Respects: <press F>**
+**S!unmute <user>**
+Unmutes a user
+
+**S!tempmute <user>**
+Temporarily Mutes A User
+
+**S!ban <user>**
+Bans A User From The Server
+
+**S!unban <userID>**
+Unbans A User From The Server
+
+**S!kick <user>**
+Kicks A User From The Server
+
+**S!clear <amount>**
+Clears an Amount Of Messages } {color:b200ff}
+]
+ `
+});
+
+bot.awaitedCommand({
+  name: "awaitReaction3",
+  code: `
+ $editMessage[$message[1];
+{title:Spoidy Fun Commands} {description:
+**S!lovecheck <@user>**
+Check Love Between You And The Username
 
 **S!say <message>**
 Make The Bot Say For You
@@ -104,30 +163,7 @@ Make The Bot Say For You In Embed
 Hacks A User For Fun
 
 **S!rps**
-Play Rock,Paper,Scissors With The Bot } {color:b200ff}
-]
- `
-});
-
-bot.awaitedCommand({
-  name: "awaitReaction3",
-  code: `
- $editMessage[$message[1];
-{title:Spoidy Inforormation Commands} {description:
-**S!help** 
-Shows The Help Commands
-
-**S!stats**
-Shows iSteal's Stats
-
-**S!botinfo**
-Shows What Is The Bot About
-
-**S!support**
-Gives Link To Support Server
-
-**S!invite**
-Gives Invite Link For iSteal
+Play Rock,Paper,Scissors With The Bot
 } {color:b200ff}]
  `
 });
