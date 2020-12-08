4daed6da75c09e5ef5620feff6a1346cb9ca1aa4
TrueGamerTalha <69008390+TrueGamerTalha@users.noreply.github.com>
2020-12-08 17:59:20 +0600

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,6 +1,8 @@
 module.exports = {
   name: "clear",
   code: `$onlyPerms[managemessages;You require Manage Messages permission for this to work.]
-$deletecommand
+  $title[**$username** Has Cleared $message Messages]
+  $color[b200ff]
+$deleteIn[5s]
 $clear[$message]`
 };
