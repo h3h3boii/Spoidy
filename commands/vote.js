70d42950bfe227d18bb340d74651c52f147b2c74
TrueGamerTalha <69008390+TrueGamerTalha@users.noreply.github.com>
2020-12-08 17:49:52 +0600

    Create vote.js

---
+++ commands/vote.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "vote",
+  code: `$channelSendMessage[$channelID;{title:Vote For **Spoidy**}
+  {description:**Vote On DiscordBotList:**
+  https://discord.ly/spoidy
+  }{color:b200ff}]`
+}
