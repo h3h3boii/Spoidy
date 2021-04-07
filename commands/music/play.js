4295db9e8db30dc51e395b75f032045514ac7192
TrueGamerTalha <69008390+TrueGamerTalha@users.noreply.github.com>
2021-04-07 23:09:32 +0600

    Add files via upload

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,8 +1,8 @@
 module.exports = ({
     name: "play",
     code: `
-    $author[Added Song]
-    $description[Added **$playSong[$noMentionMessage;1h;yes;yes;{title:Error} {description:Something went wrong!} {color:$getVar[embedc]}] **onto queue!]
+    $title[$customEmoji[spoidyhelp] Song Successfully Added!]
+    $description[Added **$playSong[$noMentionMessage;1m;yes;yes;{title:Error} {description:Something went wrong!} {color:$getVar[embedc]}] **onto queue!]
     $color[$getVar[embedc]]
     $footer[Requested by $userTag[$authorID]]
     $onlyIf[$voiceID!=;{title:Error!} {description:You need to connect to a voice channel!} {color:$getVar[embedc]}]`
