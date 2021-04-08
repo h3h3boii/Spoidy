b16e5b618f26584475067fe7aaa50e9f9c6b0af5
h3h3boii <80233365+h3h3boii@users.noreply.github.com>
2021-04-08 12:59:54 +0700

    Update reload.js

--- commands/owneronly/reload.js
+++ commands/owneronly/reload.js
@@ -4,5 +4,5 @@ aliases: ["restart"],
 code: `$updateCommands
 $customEmoji[sloading] Loading all commands.....
 $editIn[5s;$customEmoji[spoidyhelp] All commands have been loaded Successfully]
-$onlyForIDs[613963112726659092;You are not authorized to run this command!]`
+$onlyForIDs[802064404132003890;You are not authorized to run this command!]`
 });
