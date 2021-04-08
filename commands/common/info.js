c5f36ebb5563d127fc664d4452c61fd601781a7b
h3h3boii <80233365+h3h3boii@users.noreply.github.com>
2021-04-08 12:53:48 +0700

    Update info.js

--- commands/common/info.js
+++ commands/common/info.js
@@ -1,9 +1,9 @@
  module.exports = {
   name: "info",
   aliases: ["i", "botinfo"],
-  code: `$title[$customEmoji[spoidyhelp] **Spoidy's Information**]
+  code: `$title[$customEmoji[spoidyhelp] **Suparmin's Information**]
 $color[$getVar[embedc]]
-$addField[$customEmoji[sdeveloper] **Developers**;<@!613963112726659092>]
+$addField[$customEmoji[sdeveloper] **Developers**;<@!829594806774267924>]
 $addField[$customEmoji[sDISCORD] **Members**;$allMembersCount]
 $addField[$customEmoji[sserver] **Servers**;$serverCount]
 $addField[$customEmoji[sinfinite] **Uptime**;$uptime]
