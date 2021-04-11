4ff5c5a9e33171baa6acf8ec838d41f3deb5b846
h3h3boii <80233365+h3h3boii@users.noreply.github.com>
2021-04-11 23:28:00 +0700

    Update invite.js

--- commands/common/invite.js
+++ commands/common/invite.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "invite",
   code: `
-$title[Click Here To Invite Suparmin!;https://discord.com/oauth2/authorize?client_id=806345212531572766&permissions=8&scope=bot]
+$title[Click Here To Invite Suparmin!;https://discord.com/api/oauth2/authorize?client_id=806345212531572766&permissions=8&scope=bot]
 $color[$getVar[embedc]]`
 };
